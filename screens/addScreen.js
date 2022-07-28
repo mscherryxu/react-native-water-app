f7d96f975778e95d0cf6a45eaab161602fe11348
Cherry Xu <ms.cherry.xu@gmail.com>
2022-07-27 23:11:17 -0400

    Updated functionality for adding water and buttons

--- screens/addScreen.js
+++ screens/addScreen.js
@@ -1,52 +1,35 @@
-import { StyleSheet, View, Button, Text } from 'react-native';
-import waterReducer, {
-  updateConsumption,
-  resetGoal,
-  setGoal,
-} from '../src/store/waterReducer';
-import React, { useState, useEffect } from 'react';
+import {
+    Text,
+    StyleSheet,
+    View,
+    Dimensions,
+    ActivityIndicator,
+  } from "react-native";
 
-export function AddScreen() {
-  const [currentWater, setCurrentWater] = useState(() => setInitialWater());
-
-  function setInitialWater() {
-    return 0;
-  }
-
-  function addCup() {
-    return setCurrentWater(currentWater + 8);
-  }
-
-  function addCan() {
-    return setCurrentWater(currentWater + 12);
-  }
-
-  function addBottle() {
-    return setCurrentWater(currentWater + 16);
-  }
-  return (
+return (
     <View style={styles.wholeScreen}>
-      <Text>Temp Water Name</Text>
-      <View style={styles.container}>
-        <View style={styles.goal}></View>
-        <View style={styles.body}>
-          <View style={styles.waterContainer}>
-            <View>
-              <Text style={styles.waterLevel}>{currentWater}</Text>
+        <View style={styles.container}>
+            <View style={styles.goal}>
             </View>
-            <View style={styles.waterLabel}></View>
-          </View>
-          <View style={styles.waterButtons}>
-            <Button title={`Add Cup (8oz / 240mL)`} onPress={addCup} />
-            <Button title={`Add Can (12oz / 360mL)`} onPress={addCan} />
-            <Button title={`Add Bottle (16oz / 480mL)`} onPress={addBottle} />
-          </View>
+            <View style={styles.body}>
+                <View style={styles.waterContainer}>
+                    <WaterHolder waterLevel={waterLevel}>
+                        <WaterLevel style={styles.waterLevel}></WaterLevel>
+                    </WaterHolder>
+                    <WaterLabel style={styles.waterLabel}></WaterLabel>
+                </View>
+                <View style={styles.waterButtons}>
+                    <Button1 />
+                    <Button2 />
+                    <Button3 />
+                </View>
+            </View>
         </View>
-      </View>
-      <View style={styles.resetButton}>{/* <ResetButton /> */}</View>
+        <View style={styles.resetButton}>
+            <ResetButton />
+        </View>
     </View>
-  );
-}
+)
 
 //button 1, 2, 3
 /* on press, button will change color and water level will increase based on the button pressed */
@@ -58,66 +41,66 @@ export function AddScreen() {
 /* will display the water breakpoints at 0, 25, 50, 75, 100% 
 on press changes from oz to mL */
 
-//WaterLevel
+//WaterLevel 
 /* will grow vertically based on the water level */
 
 const styles = StyleSheet.create({
-  wholeScreen: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  goal: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  body: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  waterContainer: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  waterLevel: {
-    //make match water container eventually
-    //height: 100????
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  waterLabel: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  waterButtons: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-  resetButton: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    wholeScreen: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    goal: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    body: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    waterContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    waterLevel: {
+        //make match water container eventually
+        //height: 100????
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    waterLabel: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    waterButtons: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+    resetButton: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
 });
